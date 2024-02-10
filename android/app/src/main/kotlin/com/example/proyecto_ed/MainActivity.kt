9bbe4c9985c14b69e1e69d6b30453f794f04034e
onofrela <arroyoonofrel@gmail.com>
2024-02-09 19:33:31 -0600

    Archivos Base

---
+++ android/app/src/main/kotlin/com/example/proyecto_ed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proyecto_ed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Bottom Text'),
+      ),
+    );
+  }
+}
